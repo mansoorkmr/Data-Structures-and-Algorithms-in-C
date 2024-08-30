fca5da143d6768a4c2e456667a6ebcda9410856f
Mansoor Wani <164328814+mansoorkmr@users.noreply.github.com>
2024-08-30 13:16:00 +0530

    Add files via upload

---
+++ Circular linked list/circular_linked_list.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "linked_list.h"
+#include "circular_linked_list.h"
+
+/**
+ *
+ */
+CircularLinkedList createCLL(const int data)
+{
+    CircularLinkedList list;
+    list.last = malloc(sizeof(LinkedListNode));
+    list.last->data = data;
+    list.last->next = list.last;
+
+    return list;
+}
+
+/**
+ *
+ */
+void insertAtTheEndCLL(
+    CircularLinkedList* list,
+    const int data
+)
+{
+    insertAtTheBeginningCLL(
+        list,
+        data
+    );
+
+    list->last = list->last->next;
+}
+
+/**
+ *
+ */
+void insertAtTheBeginningCLL(
+    CircularLinkedList* list,
+    const int data
+)
+{
+    LinkedListNode* lastNode = list->last;
+    LinkedListNode* first = lastNode->next;
+
+    lastNode->next = malloc(sizeof(LinkedListNode));
+    lastNode->next->data = data;
+    lastNode->next->next = first;
+}
+
+/**
+ *
+ */
+const int atCLL(
+    CircularLinkedList* list,
+    const unsigned short index
+)
+{
+    LinkedListNode* node = list->last->next;
+
+    for(
+        unsigned short i = 0;
+        i < index;
+        i++
+    )
+    {
+        node = node->next;
+    }
+
+    return node->data;
+}
---
+++ Circular linked list/circular_linked_list.h
@@ -0,0 +1,55 @@
+/**
+ * @brief Circular linked list structure and methods
+ * @file circular_linked_list.h
+ */
+
+typedef struct LinkedListNode LinkedListNode;
+
+typedef struct CircularLinkedList
+{
+    struct LinkedListNode* last;
+} CircularLinkedList;
+
+/**
+ * @brief Creates a new circular linked list with one node
+ *
+ * @param data integer to set into the new created node
+ *
+ * @return CircularLinkedList
+ */
+CircularLinkedList createCLL(const int data);
+
+/**
+ * @brief Inserts one node at the end of the list
+ *
+ * @param list the circular linked list to update
+ * @param data the new data to insert
+ */
+void insertAtTheEndCLL(
+    CircularLinkedList* list,
+    const int data
+);
+
+/**
+ * @brief Inserts one node at the beginning of the list
+ *
+ * @param list the circular linked list to update
+ * @param data the new data to insert
+ */
+void insertAtTheBeginningCLL(
+    CircularLinkedList* list,
+    const int data
+);
+
+/**
+ * @brief Returns the data at the given index
+ *
+ * @param list the circular linked list to update
+ * @param index the index of the expected data
+ *
+ * @return const int
+ */
+const int atCLL(
+    CircularLinkedList* list,
+    const unsigned short index
+);
